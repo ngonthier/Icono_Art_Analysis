8017fb0423ba42acb65360fb91266d1e0684f53f
Nicolas Gonthier <nicolas.gonthier@telecom-paristech.fr>
2019-03-08 16:23:52 +0100

    ajout de PCAuse or not pour misvm et MISVM

--- Classif_Paintings/WatercolorBaseline.py
+++ Classif_Paintings/WatercolorBaseline.py
@@ -25,20 +25,22 @@ def BaselineRunAll():
                 PCA_tab = [True,False]
             else:
                 GS_tab = [False]
-                PCA_tab = [True]
+                PCA_tab = [True,False]
             for GS in GS_tab:
                 for PCAuse in PCA_tab:
                     Baseline_FRCNN_TL_Detect(demonet = 'res152_COCO',database =database,Test_on_k_bag=False,
                             normalisation= normalisation,baseline_kind=method,verbose=False,
                             gridSearch=GS,k_per_bag=300,n_jobs=4,PCAuse=PCAuse,variance_thres= variance_thres,
                             restarts=restarts,max_iter=max_iter,reDo=False)
+    PCA_tab = [True,False]
     restarts = 10
     for database in datasets:
         for method in ['MISVM','miSVM']:
-            Baseline_FRCNN_TL_Detect(demonet = 'res152_COCO',database =database,Test_on_k_bag=False,
-                    normalisation= normalisation,baseline_kind=method,verbose=False,
-                    gridSearch=False,k_per_bag=300,n_jobs=4,PCAuse=PCAuse,variance_thres= variance_thres,
-                    restarts=restarts,max_iter=max_iter,reDo=False)
+            for PCAuse in PCA_tab:
+                Baseline_FRCNN_TL_Detect(demonet = 'res152_COCO',database =database,Test_on_k_bag=False,
+                        normalisation= normalisation,baseline_kind=method,verbose=False,
+                        gridSearch=False,k_per_bag=300,n_jobs=4,PCAuse=PCAuse,variance_thres= variance_thres,
+                        restarts=restarts,max_iter=max_iter,reDo=False)
             
 if __name__ == '__main__':
     BaselineRunAll()
