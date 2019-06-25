2423cb91aa3abd2d6c8464ae09f7c1c3aa73ea65
gonthier <nicolas.gonthier@telecom-paristech.fr>
2019-06-25 18:29:11 +0200

    ajout de MI_net et companie pour tester sur nos features, pas encore fini ni tester

---
+++ Classif_Paintings/MILnet_eval.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 15:02:50 2019
+
+The goal of this script is to evaluate the Revisiting MIL network on our case of 
+WSOD
+
+@author: gonthier
+"""
+
+from MINNpy3.mil_nets.WSOD_datasets import load_dataset
+from MINNpy3.MI_Net import MI_Net_WSOD
+from MINNpy3.MI_Net_with_DS import MI_Net_with_DS_WSOD
+from MINNpy3.MI_Net_with_RC import MI_Net_with_RC_WSOD
+from MINNpy3.mi_Net import mi_Net_WSOD
+
+from IMDB_study import getDictFeaturesFasterRCNN,getTFRecordDataset
+from IMDB import get_database
+import numpy as np
+import cv2
+from tf_faster_rcnn.lib.model.test import get_blobs
+import tensorflow as tf
+from tf_faster_rcnn.lib.datasets.factory import get_imdb
+from Transform_Box import py_cpu_modif
+import pickle
+from tf_faster_rcnn.lib.model.nms_wrapper import nms
+import os 
+from LatexOuput import arrayToLatex
+from sklearn.metrics import average_precision_score
+
+MILmodel_tab = ['MI_Net','mi_Net','MI_Net_with_DS','MI_Net_with_RC']
+
+def mainEval(dataset_nm='IconArt_v1',classe=0,k_per_bag = 300,metamodel = 'FasterRCNN',\
+             demonet='res152_COCO',test=False,MILmodel='MI_Net'):
+    
+    dataset_nm='IconArt_v1'
+    classe=1
+    k_per_bag = 300
+    metamodel = 'FasterRCNN'
+    demonet='res152_COCO'
+    test=False
+    MILmodel='MI_Net'
+    if test:
+        classe = 0
+    
+    if MILmodel=='MI_Net':
+        MILmodel_fct = MI_Net_WSOD
+    elif MILmodel=='mi_Net':
+        MILmodel_fct = mi_Net_WSOD
+    elif MILmodel=='MI_Net_with_DS':
+        MILmodel_fct = MI_Net_with_DS_WSOD
+    elif MILmodel=='MI_Net_with_RC':
+        MILmodel_fct = MI_Net_with_RC_WSOD
+    else:
+        print(MILmodel,'is unkwon')
+        return(0)
+        
+    item_name,path_to_img,classes,ext,num_classes,str_val,df_label,path_data,Not_on_NicolasPC = get_database(dataset_nm)
+    
+    dataset,bags_full_label,mean_fea,std_fea = load_dataset(dataset_nm,classe=0,k_per_bag = k_per_bag,metamodel = metamodel,demonet=demonet)
+    model_dict = {}
+    
+    for j in range(num_classes):
+        if test: 
+            continue
+        else:
+            for k in range(len(dataset['train'])):
+                a = list(dataset['train'][k])
+                a[1] = [bags_full_label[k,j]]*k_per_bag
+                a = tuple(a)
+                dataset['train'][k] = a
+                
+        print('start training for class',j)
+        model = MILmodel_fct(dataset,max_epoch=1)
+        model_dict[j] = model
+    
+    dict_name_file = getDictFeaturesFasterRCNN(dataset_nm,k_per_bag=k_per_bag,\
+                                               metamodel=metamodel,demonet=demonet)
+    
+    name_file = dict_name_file['test']
+    if metamodel=='EdgeBoxes':
+        dim_rois = 4
+    else:
+        dim_rois = 5
+    next_element = getTFRecordDataset(name_file,k_per_bag =k_per_bag,dim_rois = dim_rois)
+    
+    dont_use_07_metric = False
+    if dataset_nm=='VOC2007':
+        imdb = get_imdb('voc_2007_test')
+        imdb.set_force_dont_use_07_metric(dont_use_07_metric)
+        num_images = len(imdb.image_index)
+    elif dataset_nm=='watercolor':
+        imdb = get_imdb('watercolor_test')
+        imdb.set_force_dont_use_07_metric(dont_use_07_metric)
+        num_images = len(imdb.image_index)
+    elif dataset_nm=='PeopleArt':
+        imdb = get_imdb('PeopleArt_test')
+        imdb.set_force_dont_use_07_metric(dont_use_07_metric)
+        num_images = len(imdb.image_index)
+    elif dataset_nm=='clipart':
+        imdb = get_imdb('clipart_test')
+        imdb.set_force_dont_use_07_metric(dont_use_07_metric)
+        num_images = len(imdb.image_index) 
+    elif dataset_nm=='IconArt_v1' or dataset_nm=='RMN':
+        imdb = get_imdb('IconArt_v1_test')
+        imdb.set_force_dont_use_07_metric(dont_use_07_metric)
+        num_images =  len(df_label[df_label['set']=='test'][item_name])
+    elif 'IconArt_v1' in dataset_nm and not('IconArt_v1' ==dataset_nm):
+        imdb = get_imdb('IconArt_v1_test',ext=dataset_nm.split('_')[-1])
+        imdb.set_force_dont_use_07_metric(dont_use_07_metric)
+#        num_images = len(imdb.image_index) 
+        num_images =  len(df_label[df_label['set']=='test'][item_name])
+    elif dataset_nm in ['WikiTenLabels','MiniTrain_WikiTenLabels','WikiLabels1000training']:
+        imdb = get_imdb('WikiTenLabels_test')
+        imdb.set_force_dont_use_07_metric(dont_use_07_metric)
+        #num_images = len(imdb.image_index) 
+        num_images =  len(df_label[df_label['set']=='test'][item_name])
+    else:
+        num_images =  len(df_label[df_label['set']=='test'][item_name])
+    all_boxes = [[[] for _ in range(num_images)] for _ in range(num_classes)]
+    
+    
+    TEST_NMS = 0.3
+    thresh = 0.0
+    true_label_all_test = []
+    predict_label_all_test = []
+    name_all_test = []
+    config = tf.ConfigProto()
+    config.intra_op_parallelism_threads = 16
+    config.inter_op_parallelism_threads = 16
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    i = 0
+    num_features = 2048
+    while True:
+        try:
+            fc7s,roiss,rois_scores,labels,name_imgs = sess.run(next_element)
+            fc7s = np.divide(fc7s-mean_fea, std_fea).astype(np.float32)
+            true_label_all_test += [labels]
+            score_all = None
+            for j in range(num_classes):
+                if not(test):
+                    model= model_dict[j]
+                    predictions = model.predict(fc7s.reshape((-1,num_features)),batch_size=1)
+                    scores_all_j_k = predictions.reshape((fc7s.shape[0],1,fc7s.shape[1]))
+                else:
+                    if j==classe:
+                        model= model_dict[j]
+                        scores_all_j_k = model.predict(fc7s.reshape((-1,num_features))).reshape((fc7s.shape[0],1,fc7s.shape[1]))
+                if score_all is None:
+                    score_all = scores_all_j_k
+                else:
+                    score_all = np.concatenate((score_all,scores_all_j_k),axis=1)
+            predict_label_all_test +=  [np.max(score_all,axis=2)]
+            
+            for k in range(len(labels)):
+                name_im = name_imgs[k].decode("utf-8")
+                complet_name = path_to_img + str(name_im) + '.jpg'
+                im = cv2.imread(complet_name)
+                blobs, im_scales = get_blobs(im)
+                roi = roiss[k,:]
+                if metamodel=='EdgeBoxes':
+                    roi_boxes =  roi / im_scales[0] 
+                else:
+                    roi_boxes =  roi[:,1:5] / im_scales[0]
+                
+                for j in range(num_classes):
+                    scores = score_all[k,j,:]
+                    #print(j,'scores',scores.shape)
+                    inds = np.where(scores > thresh)[0]
+                    cls_scores = scores[inds]
+                    cls_boxes = roi_boxes[inds,:]
+                    cls_dets = np.hstack((cls_boxes, cls_scores[:, np.newaxis])).astype(np.float32, copy=False)
+                    keep = nms(cls_dets, TEST_NMS)
+                    cls_dets = cls_dets[keep, :]
+                    all_boxes[j][i] = cls_dets
+                i += 1
+            for l in range(len(name_imgs)): 
+                if dataset_nm in ['IconArt_v1','VOC2007','watercolor','clipart','WikiTenLabels','PeopleArt','MiniTrain_WikiTenLabels','WikiLabels1000training']:
+                    name_all_test += [[str(name_imgs[l].decode("utf-8"))]]
+                else:
+                    name_all_test += [[name_imgs[l]]]
+                    
+        except tf.errors.OutOfRangeError:
+            break
+
+    sess.close()
+    
+    true_label_all_test = np.concatenate(true_label_all_test)
+    predict_label_all_test = np.concatenate(predict_label_all_test,axis=0)
+    name_all_test = np.concatenate(name_all_test)
+    
+    AP_per_class = []
+    for j,classe in enumerate(classes):
+            AP = average_precision_score(true_label_all_test[:,j],predict_label_all_test[:,j],average=None)
+            AP_per_class += [AP]
+    print('Average Precision classification task :')
+    print(arrayToLatex(AP_per_class,per=True))        
+            
+    max_per_image = 100
+    num_images_detect = len(imdb.image_index)  # We do not have the same number of images in the WikiTenLabels or IconArt_v1 case
+    all_boxes_order = [[[] for _ in range(num_images_detect)] for _ in range(imdb.num_classes)]
+    number_im = 0
+    name_all_test = name_all_test.astype(str)
+    for i in range(num_images_detect):
+        print(i)
+        name_img = imdb.image_path_at(i)
+        if dataset_nm=='PeopleArt':
+            name_img_wt_ext = name_img.split('/')[-2] +'/' +name_img.split('/')[-1]
+            name_img_wt_ext_tab =name_img_wt_ext.split('.')
+            name_img_wt_ext = '.'.join(name_img_wt_ext_tab[0:-1])
+        else:
+            name_img_wt_ext = name_img.split('/')[-1]
+            name_img_wt_ext =name_img_wt_ext.split('.')[0]
+        name_img_ind = np.where(np.array(name_all_test)==name_img_wt_ext)[0]
+        #print(name_img_ind)
+        if len(name_img_ind)==0:
+            print('len(name_img_ind), images not found in the all_boxes')
+            print(name_img_wt_ext)
+            raise(Exception)
+        else:
+            number_im += 1 
+#        print(name_img_ind[0])
+        for j in range(1, imdb.num_classes):
+            j_minus_1 = j-1
+            if len(all_boxes[j_minus_1][name_img_ind[0]]) >0:
+                all_boxes_order[j][i]  = all_boxes[j_minus_1][name_img_ind[0]]
+        if max_per_image > 0 and len(all_boxes_order[j][i]) >0: 
+            image_scores = np.hstack([all_boxes_order[j][i][:, -1]
+                        for j in range(1, imdb.num_classes)])
+            if len(image_scores) > max_per_image:
+                image_thresh = np.sort(image_scores)[-max_per_image]
+                for j in range(1, imdb.num_classes):
+                    keep = np.where(all_boxes_order[j][i][:, -1] >= image_thresh)[0]
+                    all_boxes_order[j][i] = all_boxes_order[j][i][keep, :]
+    assert (number_im==num_images_detect) # To check that we have the all the images in the detection prediction
+    det_file = os.path.join(path_data, 'detections.pkl')
+    with open(det_file, 'wb') as f:
+        pickle.dump(all_boxes_order, f, pickle.HIGHEST_PROTOCOL)
+    output_dir = path_data +'tmp/' + dataset_nm+'_mAP.txt'
+    aps =  imdb.evaluate_detections(all_boxes_order, output_dir)
+    apsAt05 = aps
+    print("Detection score (thres = 0.5): ",dataset_nm)
+    print(arrayToLatex(aps,per=True))
+    ovthresh_tab = [0.3,0.1,0.]
+    for ovthresh in ovthresh_tab:
+        aps = imdb.evaluate_localisation_ovthresh(all_boxes_order, output_dir,ovthresh)
+        if ovthresh == 0.1:
+            apsAt01 = aps
+        print("Detection score with thres at ",ovthresh,'with ',model)
+        print(arrayToLatex(aps,per=True))
+    
+    return(apsAt05,apsAt01,AP_per_class)
+
+if __name__ == '__main__':
+    mainEval()
---
+++ Classif_Paintings/MINNpy3/mil_nets/WSOD_datasets.py
@@ -0,0 +1,123 @@
+import numpy as np
+import scipy.io as sio
+from sklearn.model_selection import KFold
+
+import tensorflow as tf
+
+from tf_faster_rcnn.lib.datasets.factory import get_imdb
+from IMDB_study import getDictFeaturesFasterRCNN,getTFRecordDataset
+from IMDB import get_database
+
+def load_dataset(dataset_nm='IconArt_v1',set_='train',classe=0,k_per_bag = 300,metamodel = 'FasterRCNN',demonet='res152_COCO'):
+    """Load data from file, do pre-processing, split it into train/test set.
+    Parameters
+    -----------------
+    dataset_nm : string
+        Name of dataset.
+    Returns
+    -----------------
+    datasets : list Train and test
+    """
+    # load data from file
+    
+    datasets = {}
+    
+    item_name,path_to_img,classes,ext,num_classes,str_val,df_label,path_data,Not_on_NicolasPC = get_database(dataset_nm)
+    
+    dict_name_file = getDictFeaturesFasterRCNN(dataset_nm,k_per_bag=k_per_bag,\
+                                               metamodel=metamodel,demonet=demonet)
+    
+    config = tf.ConfigProto()
+    config.intra_op_parallelism_threads = 16
+    config.inter_op_parallelism_threads = 16
+    config.gpu_options.allow_growth = True
+    
+    ins_fea = None
+    bags_full_label = None
+    bags_nm = None
+    bags_nm = 0 
+    if set_=='train':
+        name_file = dict_name_file[set_]
+        if metamodel=='EdgeBoxes':
+            dim_rois = 4
+        else:
+            dim_rois = 5
+        next_element = getTFRecordDataset(name_file,k_per_bag =k_per_bag,dim_rois = dim_rois)
+        
+
+        sess = tf.Session(config=config)
+        while True:
+            try:
+                fc7s,roiss,rois_scores,labels,name_imgs = sess.run(next_element)
+                if ins_fea is None:
+                    ins_fea = fc7s.astype(np.float32)
+                    bags_full_label = labels.astype(np.float32)
+                    bags_nm = name_imgs
+                else:
+                    ins_fea = np.vstack((ins_fea,fc7s)).astype(np.float32)
+                    bags_full_label = np.vstack((bags_full_label,labels)).astype(np.float32)
+                    bags_nm = np.concatenate((bags_nm,name_imgs))
+                #for k in range(len(labels)):
+                    #name_im = name_imgs[k].decode("utf-8")
+                    
+            except tf.errors.OutOfRangeError:
+                break
+    
+        sess.close()
+        
+    bags_full_label = np.array(bags_full_label)
+    bags_label = bags_full_label[:,classe] 
+
+#    data = sio.loadmat('./dataset/'+dataset_nm+'.mat')
+#    ins_fea = data['x']['data'][0,0]
+#    if dataset_nm.startswith('musk'):
+#        bags_nm = data['x']['ident'][0,0]['milbag'][0,0]
+#    else:
+#        bags_nm = data['x']['ident'][0,0]['milbag'][0,0][:,0]
+#    bags_label = data['x']['nlab'][0,0][:,0] - 1
+
+    # L2 norm for musk1 and musk2
+
+    if dataset_nm.startswith('newsgroups') is False:
+        mean_fea = np.mean(ins_fea, axis=(0,1), keepdims=True)+1e-6
+        std_fea = np.std(ins_fea, axis=(0,1), keepdims=True)+1e-6
+        ins_fea = np.divide(ins_fea-mean_fea, std_fea)
+    else:
+        mean_fea = np.ones((1,1,ins_fea.shape[2]))
+        std_fea = np.ones((1,1,ins_fea.shape[2]))
+        
+    bags_fea = []
+    for id, bag_nm in enumerate(bags_nm):
+        bag_fea = ([], [])
+        for ins_idx in range(k_per_bag):
+            bag_fea[0].append(ins_fea[id,ins_idx,:])
+            bag_fea[1].append(bags_label[id])
+        bags_fea.append(bag_fea)
+#        
+#    # store data in bag level
+#    ins_idx_of_input = {}            # store instance index of input
+#    for id, bag_nm in enumerate(bags_nm):
+#        if bag_nm in ins_idx_of_input: ins_idx_of_input[bag_nm].append(id)
+#        else: ins_idx_of_input[bag_nm] = [id]
+#    bags_fea = []
+#    for bag_nm, ins_idxs in list(ins_idx_of_input.items()):
+#        bag_fea = ([], [])
+#        for ins_idx in ins_idxs:
+#            print('ins_fea[ins_idx][0]',ins_fea[ins_idx][0].shape)
+#            bag_fea[0].append(ins_fea[ins_idx][0])
+#            bag_fea[1].append(bags_label[ins_idx])
+#        bags_fea.append(bag_fea)
+
+    datasets['train']= bags_fea
+    
+
+#    # random select 90% bags as train, others as test
+#    num_bag = len(bags_fea)
+#    kf = KFold(num_bag, n_folds=n_folds, shuffle=True, random_state=None)
+#    datasets = []
+#    for train_idx, test_idx in kf:
+#        
+#        dataset['train'] = [bags_fea[ibag] for ibag in train_idx]
+#        dataset['test'] = [bags_fea[ibag] for ibag in test_idx]
+#        datasets.append(dataset)
+    return datasets,bags_full_label,mean_fea,std_fea
