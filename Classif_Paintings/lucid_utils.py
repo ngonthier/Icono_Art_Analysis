4423a84e824d51b738bfec94ba925aa618bd0d1e
gonthier <nicolas.gonthier@telecom-paristech.fr>
2020-04-21 13:31:46 +0200

    correction RGB folder

--- Classif_Paintings/lucid_utils.py
+++ Classif_Paintings/lucid_utils.py
@@ -366,7 +366,7 @@ def print_images(model_path,list_layer_index_to_print,path_output='',prexif_name
         name_output = os.path.join(path_output,name_base)
         tf.keras.preprocessing.image.save_img(name_output, image)
         
-        name_output = os.path.join(name_output,'RGB')
+        name_output = os.path.join(path_output,'RGB')
         pathlib.Path(name_output).mkdir(parents=True, exist_ok=True) 
         change_from_BRG_to_RGB(img_name_path=name_output,output_path=None,ext_name='toRGB')
       
