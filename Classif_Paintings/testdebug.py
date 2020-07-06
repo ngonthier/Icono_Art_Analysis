5585094f73889b6b0d8129b698d5aa8dace4adc4
gonthier <nicolas.gonthier@telecom-paristech.fr>
2020-07-06 19:15:52 +0200

    ajout test

--- Classif_Paintings/testdebug.py
+++ Classif_Paintings/testdebug.py
@@ -14,6 +14,7 @@ learn_and_eval(target_dataset,source_dataset='ImageNet',final_clf='MLP2',feature
     regulOnNewLayer=None,optimizer='SGD',opt_option=[0.1,0.01],\
     epochs=5,SGDmomentum=0.9,decay=1e-4,batch_size=16,pretrainingModif=True,\
     suffix='testdebug',plotConv=True,verbose=True)
+
     
 # Former performance : # & 68.1 & 47.8 & 93.0 & 73.5 & 61.6 & 72.4 & 56.1 & 79.0 & 71.4 & 86.8 & 71.0 \\ 
     
@@ -30,3 +31,9 @@ learn_and_eval(target_dataset,source_dataset='ImageNet',final_clf='MLP2',feature
 #     regulOnNewLayer=None,optimizer='SGD',opt_option=[0.1,0.01],\
 #     epochs=5,SGDmomentum=0.9,decay=1e-4,batch_size=16,pretrainingModif=True,\
 #     suffix='testdebug',plotConv=True,verbose=True,clipnorm=10.)
+# learn_and_eval(target_dataset,source_dataset='ImageNet',final_clf='MLP2',features='block5_pool',\
+#     constrNet='ResNet50',kind_method='FT',gridSearch=False,ReDo=True,\
+#     transformOnFinalLayer='GlobalAveragePooling2D',cropCenter=True,\
+#     regulOnNewLayer=None,optimizer='adam',opt_option=[0.1,0.01],\
+#     epochs=5,SGDmomentum=0.9,decay=1e-4,batch_size=16,pretrainingModif=True,\
+#     suffix='testdebug',plotConv=True,verbose=True)
