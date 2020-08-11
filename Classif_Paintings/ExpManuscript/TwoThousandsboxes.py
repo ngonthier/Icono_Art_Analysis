6a6db20f4505a786a751c180df4b20c9cc8d4a1e
gonthier <nicolas.gonthier@telecom-paristech.fr>
2020-08-11 11:58:39 +0200

    experiment for PhD manuscript

--- Classif_Paintings/ExpManuscript/TwoThousandsboxes.py
+++ Classif_Paintings/ExpManuscript/TwoThousandsboxes.py
@@ -5,7 +5,8 @@ Created on Tue Aug 11 11:28:08 2020
 
 @author: gonthier
 """
-
+import sys
+sys.path.append("..")
 from Exp_ForPhDmanuscrit import TwoThousandsboxes_MIMAX
 
 if __name__ == '__main__':                                       
