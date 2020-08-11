de5b0ecd0f8d8654dba6fa9a8e6740eacdef9efe
gonthier <nicolas.gonthier@telecom-paristech.fr>
2020-08-11 13:32:27 +0200

    experiment for PhD manuscript

--- Classif_Paintings/Other_way_to_use_score_MIMAX.sh
+++ Classif_Paintings/Other_way_to_use_score_MIMAX.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /ldaphome/gonthier/Classif/Icono_Art_Analysis/Classif_Paintings/ExpManuscript/
+cd /ldaphome/gonthier/Classif/Icono_Art_Analysis/Classif_Paintings/
 source /cal/softs/anaconda/anaconda3/bin/activate TF
 /cal/softs/anaconda/anaconda3/bin/python -u Other_way_to_use_score_MIMAX.py >> results/Other_way_to_use_score_MIMAX.txt
--- Classif_Paintings/PascalVOC_sanity_check.sh
+++ Classif_Paintings/PascalVOC_sanity_check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /ldaphome/gonthier/IconArtAnalysis/Classif_Paintings/ExpManuscript/
+cd /ldaphome/gonthier/IconArtAnalysis/Classif_Paintings/
 source /cal/softs/anaconda/anaconda3/bin/activate tf18
 /cal/softs/anaconda/anaconda3/bin/python -u PascalVOC_sanity_check.py >> results/PascalVOC_sanity_check.txt
