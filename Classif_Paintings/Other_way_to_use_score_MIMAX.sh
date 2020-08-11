1507b4cbf0b8ddee1a2914540a732e6a9e482bbd
gonthier <nicolas.gonthier@telecom-paristech.fr>
2020-08-11 13:37:50 +0200

    error for cluster

--- Classif_Paintings/Other_way_to_use_score_MIMAX.sh
+++ Classif_Paintings/Other_way_to_use_score_MIMAX.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /ldaphome/gonthier/Classif/Icono_Art_Analysis/Classif_Paintings/
+cd /ldaphome/gonthier/Icono_Art_Analysis/Classif_Paintings/
 source /cal/softs/anaconda/anaconda3/bin/activate tf18
 /cal/softs/anaconda/anaconda3/bin/python -u Other_way_to_use_score_MIMAX.py >> results/Other_way_to_use_score_MIMAX.txt
