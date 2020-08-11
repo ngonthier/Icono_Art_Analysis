ab6ac6289eaf5b3bfa9e63f2ec97346d5a9b0212
gonthier <nicolas.gonthier@telecom-paristech.fr>
2020-08-11 11:47:29 +0200

    experiment for PhD manuscript

--- Classif_Paintings/ExpManuscript/TwoThousandsboxes_MIMAX.sh
+++ Classif_Paintings/ExpManuscript/TwoThousandsboxes_MIMAX.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /ldaphome/gonthier/Classif/Icono_Art_Analysis/Classif_Paintings/ExpManuscript/
 source /cal/softs/anaconda/anaconda3/bin/activate TF
-/cal/softs/anaconda/anaconda3/bin/python -u TwoThousandsboxes_MIMAX.py >> results/TwoThousandsboxes_MIMAX.txt
+/cal/softs/anaconda/anaconda3/bin/python -u TwoThousandsboxes.py >> results/TwoThousandsboxes.txt
