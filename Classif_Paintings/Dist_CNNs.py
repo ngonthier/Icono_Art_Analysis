67968f969a99a83abb2a64b4549cd08a442cd32f
gonthier <nicolas.gonthier@telecom-paristech.fr>
2020-10-08 13:08:21 +0200

    remove div

--- Classif_Paintings/Dist_CNNs.py
+++ Classif_Paintings/Dist_CNNs.py
@@ -907,8 +907,8 @@ def comp_cka_for_paper():
         print(netA,netB,dico)
         
     # Paintings dataset 
-    list_models_name_I = ['IconArt_v1_small01_modif',
-                        'IconArt_v1_big01_modif',
+    # IconArt_v1_small01_modif diverge 
+    list_models_name_I = ['IconArt_v1_big01_modif',
                         'IconArt_v1_big001_modif',
                         'IconArt_v1_big001_modif_XXRASTA_big0001_modif_adam_unfreeze50_RandForUnfreezed_SmallDataAug_ep200XX',
                         'IconArt_v1_big01_modif_XXRASTA_big0001_modif_adam_unfreeze50_RandForUnfreezed_SmallDataAug_ep200XX',
