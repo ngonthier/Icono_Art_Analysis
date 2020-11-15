b0138e8d953dfb2e689161e1b76059862d2097de
gonthier <nicolas.gonthier@telecom-paristech.fr>
2020-11-15 21:04:00 +0100

    other exp

--- Classif_Paintings/MoreExp_PhDmanuscript_FT.py
+++ Classif_Paintings/MoreExp_PhDmanuscript_FT.py
@@ -10,43 +10,49 @@ Experiences en plus a faire tourner pour la these :
 
 from CompNet_FT_lucidIm import Comparaison_of_FineTunedModel,print_performance_FineTuned_network
 
+from StatsConstr_ClassifwithTL import learn_and_eval
+from keras_resnet_utils import getBNlayersResNet50
+
 ### Cas de Paintings et IconArt pour fine-tune les modeles et voir les performances
 
-def print_IconArtv1_performance(latexOutput=True):
+def print_IconArtv1_performance(latexOutput=False):
     # For Classification performance different setup
+    # 'IconArt_v1_big01_modif_GAP', diverge
     list_models_name_VGG = ['IconArt_v1_small01_modif_GAP',
-                            'IconArt_v1_big01_modif_GAP',
                             'IconArt_v1_big001_modif_GAP',
                             'IconArt_v1_big0001_modif_GAP_adam_unfreeze8_RandForUnfreezed_SmallDataAug_ep200',
-                            'IconArt_v1_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedG',
+                            'IconArt_v1_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedG',
                             ]
 
     print_performance_FineTuned_network(constrNet='VGG',
                                         list_models_name=list_models_name_VGG,
                                         suffix_tab=[''],latexOutput=latexOutput)
+    
     list_models_name_ResNet = ['IconArt_v1_small01_modif_GAP',
                             'IconArt_v1_big01_modif_GAP',
                             'IconArt_v1_big001_modif_GAP',
                             'IconArt_v1_big0001_modif_GAP_adam_unfreeze20_RandForUnfreezed_SmallDataAug_ep200',
-                            'IconArt_v1_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedG',
+                            'IconArt_v1_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedG',
                             ]
 
     print_performance_FineTuned_network(constrNet='ResNet50',
                                         list_models_name=list_models_name_ResNet,
                                         suffix_tab=[''],latexOutput=latexOutput)
     
-#    list_models_name=['IconArt_v1_big001_modif_adam_unfreeze84_SmallDataAug_ep200',
-#                      'IconArt_v1_big001_modif_Adadelta_unfreeze84_MediumDataAug_ep200']
-#    print_performance_FineTuned_network(constrNet='InceptionV1_slim',
-#                                        list_models_name=list_models_name,
-#                                        suffix_tab=[''],latexOutput=latexOutput)
-def print_Paintings_performance(latexOutput=True):
+    # list_models_name=['IconArt_v1_big001_modif_adam_unfreeze84_SmallDataAug_ep200',
+    #                   'IconArt_v1_big001_modif_Adadelta_unfreeze84_MediumDataAug_ep200']
+    # print_performance_FineTuned_network(constrNet='InceptionV1_slim',
+    #                                     list_models_name=list_models_name,
+    #                                     suffix_tab=[''],latexOutput=latexOutput)
+    
+    
+def print_Paintings_performance(latexOutput=False):
     # For Classification performance different setup
+    # 'Paintings_big01_modif_GAP',  diverge
     list_models_name_VGG = ['Paintings_small01_modif_GAP',
-                            'Paintings_big01_modif_GAP',
                             'Paintings_big001_modif_GAP',
                             'Paintings_big0001_modif_GAP_adam_unfreeze8_RandForUnfreezed_SmallDataAug_ep200',
-                            'Paintings_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedG',
+                            'Paintings_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedG',
                             ]
 
     print_performance_FineTuned_network(constrNet='VGG',
@@ -56,7 +62,7 @@ def print_Paintings_performance(latexOutput=True):
                             'Paintings_big01_modif_GAP',
                             'Paintings_big001_modif_GAP',
                             'Paintings_big0001_modif_GAP_adam_unfreeze20_RandForUnfreezed_SmallDataAug_ep200',
-                            'Paintings_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedG',
+                            'Paintings_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedG',
                             ]
 
     print_performance_FineTuned_network(constrNet='ResNet50',
@@ -70,7 +76,37 @@ def print_Paintings_performance(latexOutput=True):
 #                                        suffix_tab=[''],latexOutput=latexOutput)
     
     
+### RASTA performance : 
 
+def print_RASTA_performance(latexOutput=False):
+    list_models_name = ['RASTA_small01_modif_GAP',
+                        #'RASTA_small01_modif_GAP_adam_unfreeze20_SmallDataAug',
+                        'RASTA_big0001_modif_GAP_adam_unfreeze20_RandForUnfreezed_SmallDataAug_ep200',
+                        'RASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedG'
+                        ]
+                        
+    print_performance_FineTuned_network(constrNet='ResNet50',
+                                        list_models_name=list_models_name,
+                                        suffix_tab=[''],latexOutput=latexOutput)
+    # 'RASTA_small01_modif_GAP'
+    # Top-1 accuracy : 55.99%
+    # Top-3 accuracy : 82.71%
+    # Top-5 accuracy : 91.70%
+    # ResNet50 & RASTA\_small01\_modif\_GAP & 55.99 & 82.71 & 91.70\\
+
+    #'RASTA_small01_modif_GAP_adam_unfreeze20_SmallDataAug',
+    # Top-1 accuracy : 46.81%
+    # Top-3 accuracy : 77.02%
+    # Top-5 accuracy : 87.36%
+    # ResNet50 & RASTA\_small01\_modif\_GAP\_adam\_unfreeze20\_SmallDataAug & 46.81 & 77.02 & 87.36\\
+
+
+    list_models_name = ['RASTA_small01_modif_GAP',
+                        'RASTA_big0001_modif_GAP_adam_unfreeze8_RandForUnfreezed_SmallDataAug_ep200',
+                        'RASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedG'] 
+    print_performance_FineTuned_network(constrNet='VGG',
+                                        list_models_name=list_models_name,
+                                        suffix_tab=[''],latexOutput=latexOutput)
 
 ### Cas de RASTA fine-tuning et Visualisation
 
@@ -114,7 +150,7 @@ def RASTA_ResNet_VGG_feat_vizu():
                            # 'RASTA_big01_modif_GAP',
                            # 'RASTA_big001_modif_GAP',
                             'RASTA_big0001_modif_GAP_adam_unfreeze8_RandForUnfreezed_SmallDataAug_ep200',
-                            'RASTA_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedG',
+                            'RASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedG',
                             ]
      Comparaison_of_FineTunedModel(list_model_name_4,constrNet='VGG')
 
@@ -122,7 +158,7 @@ def RASTA_ResNet_VGG_feat_vizu():
 
 ### Cas de Paintins et IconArt avec un passage intermediaire par RASTA
 
-def print_perform_Paintings_IconArt_RASTA_intermediaire(latexOutput=True):
+def print_perform_Paintings_IconArt_RASTA_intermediaire(latexOutput=False):
     
     # VGG IconArt
     list_models_name = ['IconArt_v1_small01_modif_GAP_XXRASTA_small01_modif_GAPXX',
@@ -131,9 +167,9 @@ def print_perform_Paintings_IconArt_RASTA_intermediaire(latexOutput=True):
                         'IconArt_v1_small01_modif_GAP_XXRASTA_big0001_modif_GAP_adam_unfreeze8_RandForUnfreezed_SmallDataAug_ep200XX',
                         'IconArt_v1_big01_modif_GAP_XXRASTA_big0001_modif_GAP_adam_unfreeze8_RandForUnfreezed_SmallDataAug_ep200XX',
                         'IconArt_v1_big001_modif_GAP_XXRASTA_big0001_modif_GAP_adam_unfreeze8_RandForUnfreezed_SmallDataAug_ep200XX',
-                        'IconArt_v1_small01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedGXX',
-                        'IconArt_v1_big01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedGXX',
-                        'IconArt_v1_big001_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedGXX'
+                        'IconArt_v1_small01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedGXX',
+                        'IconArt_v1_big01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedGXX',
+                        'IconArt_v1_big001_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedGXX'
                         ]
 
     print_performance_FineTuned_network(constrNet='VGG',
@@ -148,9 +184,9 @@ def print_perform_Paintings_IconArt_RASTA_intermediaire(latexOutput=True):
                         'IconArt_v1_small01_modif_GAP_XXRASTA_big0001_modif_GAP_adam_unfreeze20_RandForUnfreezed_SmallDataAug_ep200XX',
                         'IconArt_v1_big01_modif_GAP_XXRASTA_big0001_modif_GAP_adam_unfreeze20_RandForUnfreezed_SmallDataAug_ep200XX',
                         'IconArt_v1_big001_modif_GAP_XXRASTA_big0001_modif_GAP_adam_unfreeze20_RandForUnfreezed_SmallDataAug_ep200XX',
-                        'IconArt_v1_small01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedGXX',
-                        'IconArt_v1_big01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedGXX',
-                        'IconArt_v1_big001_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedGXX'
+                        'IconArt_v1_small01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedGXX',
+                        'IconArt_v1_big01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedGXX',
+                        'IconArt_v1_big001_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedGXX'
                         ]
 
     print_performance_FineTuned_network(constrNet='ResNet',
@@ -163,9 +199,9 @@ def print_perform_Paintings_IconArt_RASTA_intermediaire(latexOutput=True):
                         'Paintings_small01_modif_GAP_XXRASTA_big0001_modif_GAP_adam_unfreeze8_RandForUnfreezed_SmallDataAug_ep200XX',
                         'Paintings_big01_modif_GAP_XXRASTA_big0001_modif_GAP_adam_unfreeze8_RandForUnfreezed_SmallDataAug_ep200XX',
                         'Paintings_big001_modif_GAP_XXRASTA_big0001_modif_GAP_adam_unfreeze8_RandForUnfreezed_SmallDataAug_ep200XX',
-                        'Paintings_small01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedGXX',
-                        'Paintings_big01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedGXX',
-                        'Paintings_big001_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedGXX'
+                        'Paintings_small01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedGXX',
+                        'Paintings_big01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedGXX',
+                        'Paintings_big001_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedGXX'
                         ]
 
     print_performance_FineTuned_network(constrNet='VGG',
@@ -180,9 +216,9 @@ def print_perform_Paintings_IconArt_RASTA_intermediaire(latexOutput=True):
                         'Paintings_small01_modif_GAP_XXRASTA_big0001_modif_GAP_adam_unfreeze20_RandForUnfreezed_SmallDataAug_ep200XX',
                         'Paintings_big01_modif_GAP_XXRASTA_big0001_modif_GAP_adam_unfreeze20_RandForUnfreezed_SmallDataAug_ep200XX',
                         'Paintings_big001_modif_GAP_XXRASTA_big0001_modif_GAP_adam_unfreeze20_RandForUnfreezed_SmallDataAug_ep200XX',
-                        'Paintings_small01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedGXX',
-                        'Paintings_big01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedGXX',
-                        'Paintings_big001_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_deepSupervision_ep200_LRschedGXX'
+                        'Paintings_small01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedGXX',
+                        'Paintings_big01_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedGXX',
+                        'Paintings_big001_modif_GAP_XXRASTA_big001_modif_GAP_RandInit_randomCrop_ep200_LRschedGXX'
                         ]
 
     print_performance_FineTuned_network(constrNet='ResNet',
@@ -190,9 +226,81 @@ def print_perform_Paintings_IconArt_RASTA_intermediaire(latexOutput=True):
                                         suffix_tab=[''],latexOutput=latexOutput)
 
 
+def exp_BN_only():
+    """In this exp we only fine-tuned the batch normalization of the models"""
+    
+    target_dataset_tab = ['Paintings','IconArt_v1']
+    
+    ## VGG VGGAdaIn train only the batch normalisation
+    constrNet = 'VGGAdaIn'
+    style_layers = ['block1_conv1','block1_conv2','block2_conv1','block2_conv2',
+    'block3_conv1','block3_conv2','block3_conv3','block3_conv4',
+    'block4_conv1','block4_conv2','block4_conv3','block4_conv4', 
+    'block5_conv1','block5_conv2','block5_conv3','block5_conv4']
+    opt_option=[0.1,0.01]
+    optimizer='SGD'
+    SGDmomentum=0.9
+    decay=1e-4
+    features = 'block5_pool'
+    final_clf = 'MLP1'
+    transformOnFinalLayer='GlobalAveragePooling2D'
+    return_best_model = True
+    epochs=20
+    cropCenter = True
+    getBeforeReLU = True
+    
+    regulOnNewLayer = None
+    nesterov = False
+    dropout = None
+    onlyPlot = False
+    freezingType = 'FromBottom'
+
+    pretrainingModif = False
+    
+    for target_dataset in target_dataset_tab:
+    #            print(constrNet,style_layers)
+        metrics = learn_and_eval(target_dataset,constrNet=constrNet,kind_method='FT',\
+                                epochs=epochs,transformOnFinalLayer=transformOnFinalLayer,\
+                                forLatex=True,optimizer=optimizer,\
+                                pretrainingModif=pretrainingModif,freezingType=freezingType,\
+                                opt_option=opt_option,cropCenter=cropCenter,\
+                                style_layers=style_layers,getBeforeReLU=getBeforeReLU,\
+                                final_clf=final_clf,features=features,\
+                                return_best_model=return_best_model,\
+                                onlyReturnResult=onlyPlot,\
+                                dropout=dropout,regulOnNewLayer=regulOnNewLayer,\
+                                nesterov=nesterov,SGDmomentum=SGDmomentum,decay=decay)
+                
+    ## ResNet50 :
+        
+    constrNet = 'ResNet50AdaIn'
+    getBeforeReLU = True
+    #batch_size = 16 
+    features = 'activation_48' 
+    style_layers = getBNlayersResNet50()
+    for target_dataset in target_dataset_tab:
+#            print(constrNet,style_layers)
+        metrics = learn_and_eval(target_dataset=target_dataset,constrNet=constrNet,\
+                                 forLatex=True,
+                                 kind_method='FT',epochs=epochs,transformOnFinalLayer=transformOnFinalLayer,\
+                                 pretrainingModif=pretrainingModif,freezingType=freezingType,\
+                                 optimizer=optimizer,opt_option=opt_option, #batch_size=batch_size,\
+                                 final_clf=final_clf,features=features,return_best_model=return_best_model,\
+                                 onlyReturnResult=onlyPlot,style_layers=style_layers,
+                                 cropCenter=cropCenter,dropout=dropout,regulOnNewLayer=regulOnNewLayer,\
+                                 nesterov=nesterov,SGDmomentum=SGDmomentum,decay=decay)
+            
+    # InceptionV1 adaIn a faire !
+            
+
+
 if __name__ == '__main__': 
+    
+    #exp_BN_only()
+    #a faire plus tard
     
     print_IconArtv1_performance()
     print_Paintings_performance()
-    RASTA_ResNet_VGG_feat_vizu()
+    print_RASTA_performance()
+    #RASTA_ResNet_VGG_feat_vizu()
     print_perform_Paintings_IconArt_RASTA_intermediaire()
